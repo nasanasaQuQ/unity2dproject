9e65f0cf7c5b760d8b0496d190ed8f287c4d3e31
nasanasaQuQ <731374839@qq.com>
2021-12-13 22:20:34 +0800

    摄像机跟随player

---
+++ Script/CameraFollow.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/**
+ * 相机跟随player
+ */
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+
+    public float smoothing;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+        if (target)
+        {
+            if (transform.position != target.position)
+            {
+                Vector3 targetPos = target.position;
+                transform.position = Vector3.Lerp(transform.position, targetPos, smoothing);
+            }
+        }
+    }
+}
