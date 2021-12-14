cf712fbd882d46d3786da10ba52d9f9f87e289a1
nasanasaQuQ <731374839@qq.com>
2021-12-14 19:21:49 +0800

    限制相机移动carema limit

--- Script/CameraFollow.cs
+++ Script/CameraFollow.cs
@@ -10,16 +10,15 @@ public class CameraFollow : MonoBehaviour
     public Transform target;
 
     public float smoothing;
+    
+    // 最小移动以及最大移动坐标
+    public Vector2 minPosition;
+
+    public Vector2 maxPosition;
     // Start is called before the first frame update
     void Start()
     {
         GameController.MyCameraShake = GameObject.FindGameObjectWithTag("Shake").GetComponent<CameraShake>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     private void LateUpdate()
@@ -29,8 +28,16 @@ public class CameraFollow : MonoBehaviour
             if (transform.position != target.position)
             {
                 Vector3 targetPos = target.position;
+                targetPos.x = Mathf.Clamp(targetPos.x, minPosition.x, maxPosition.x);
+                targetPos.y = Mathf.Clamp(targetPos.y, minPosition.y, maxPosition.y);
                 transform.position = Vector3.Lerp(transform.position, targetPos, smoothing);
             }
         }
+    }
+
+    public void SetCamPosLimit(Vector2 minPos, Vector2 maxPos)
+    {
+        minPosition = minPos;
+        maxPosition = maxPos;
     }
 }
