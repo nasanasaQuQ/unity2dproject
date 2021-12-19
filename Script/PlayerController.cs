8610803624e640d5f2ebc7a4708b939821b428ab
nasanasaQuQ <731374839@qq.com>
2021-12-19 15:55:42 +0800

    实现player爬梯子

--- Script/PlayerController.cs
+++ Script/PlayerController.cs
@@ -9,17 +9,30 @@ public class PlayerController : MonoBehaviour
     public float moveSpped;
     public float jumpSpped;
     public float doubleJumpSpped;
+    public float climbSpeed;
+        
     private bool _canDoubleJump;
     private bool _isGround;
     private Rigidbody2D _rigidbody2D;
     private BoxCollider2D _boxCollider2D;
     private Animator _animator;
     private bool _isOneWayPlatform;
+
+    private bool _isLadder;
+    private bool _isClimbing;
+
+    private bool _isJump;
+    private bool _isDoubleJump;
+    private bool _isFall;
+    private bool _isDoubleFall;
+
+    private float _oldPlayerGravity;
     private void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
         _boxCollider2D = GetComponent<BoxCollider2D>();
         _animator = GetComponent<Animator>();
+        _oldPlayerGravity = _rigidbody2D.gravityScale;
     }
 
     // Update is called once per frame
@@ -33,6 +46,9 @@ public class PlayerController : MonoBehaviour
             CheckIsGround();
             SwitchAnimation();
             OneWayPlatformCheck();
+            CheckIsLadder();
+            Climb();
+            CheckAirStatus();
         }
     }
     
@@ -160,7 +176,55 @@ public class PlayerController : MonoBehaviour
 
         }
     }
-    
-    
+
+    private void CheckIsLadder()
+    {
+        _isLadder = _boxCollider2D.IsTouchingLayers(LayerMask.GetMask("Ladder"));
+    }
+
+    private void Climb()
+    {   
+        
+        if (_isLadder)
+        {
+            var moveY = Input.GetAxis("Vertical");
+            if (moveY > 0.5f || moveY < -0.5f)
+            {
+                _animator.SetBool("Climbing",true);
+                _rigidbody2D.gravityScale = 0.0f;
+                _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, moveY * climbSpeed);
+            }
+            // 没有爬梯子的情况
+            else
+            {
+                if (_isJump || _isFall || _isDoubleFall || _isDoubleJump)
+                {
+                    _animator.SetBool("Climbing",false);
+                }
+                // 停在梯子上
+                else
+                {
+                    _animator.SetBool("Climbing",false);
+                    _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, 0.0f);
+                }
+            }
+        }
+        // player不在梯子上。
+        else
+        {
+            _animator.SetBool("Climbing", false);
+            _rigidbody2D.gravityScale = _oldPlayerGravity;
+        }
+    }
+
+    private void CheckAirStatus()
+    {
+        _isJump = _animator.GetBool("Jump");
+        _isFall = _animator.GetBool("Fall");
+        _isDoubleFall = _animator.GetBool("DoubleJump");
+        _isDoubleJump = _animator.GetBool("DoubleJump");
+        _isClimbing = _animator.GetBool("Climbing");
+
+    }
     
 }
