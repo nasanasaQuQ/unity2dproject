4607e0e05404ffe3291c3c00f88342680fd0a33c
nasanasaQuQ <731374839@qq.com>
2021-12-27 21:17:04 +0800

    progress Bar

--- Script/MainMenu.cs
+++ Script/MainMenu.cs
@@ -2,8 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 public class MainMenu : MonoBehaviour
-{   
+{
+    public GameObject loading;
+
+    public Slider slider;
+
+    public Text progreassText;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +26,24 @@ public class MainMenu : MonoBehaviour
     public void PlayGame()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void LoadLevel(int sceneIndex)
+    {
+        StartCoroutine(AsyncLoadLevel(sceneIndex));
+    }
+
+    IEnumerator AsyncLoadLevel(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+        loading.SetActive(true);
+        while (!operation.isDone)
+        {
+            float progress = operation.progress / 0.9f;
+            slider.value = progress;
+            progreassText.text = Mathf.FloorToInt(progress * 100f) + "%";
+            yield return null;
+        }
     }
 
     public void QuitGame()
